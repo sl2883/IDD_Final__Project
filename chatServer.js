862e1b5f93dba3eb68e2694f27cd4bfff9398d19
Sunny Ladkani <54920880+sl2883@users.noreply.github.com>
2019-12-12 09:32:43 -0500

    final project

---
+++ chatServer.js
@@ -0,0 +1,264 @@
+/*
+chatServer.js
+Author: David Goedicke (da.goedicke@gmail.com)
+Closley based on work from Nikolas Martelaro (nmartelaro@gmail.com) as well as Captain Anonymous (https://codepen.io/anon/pen/PEVYXz) who forked of an original work by Ian Tairea (https://codepen.io/mrtairea/pen/yJapwv)
+*/
+
+var express = require('express'); // web server application
+var app = express(); // webapp
+var http = require('http').Server(app); // connects http library to server
+
+var SerialPort = require('serialport');
+var Readline = SerialPort.parsers.Readline; // read serial data as lines
+
+var io = require('socket.io')(http); // connect websocket library to server
+
+// check to make sure that the user provides the serial port for the Arduino
+// when running the server
+	if (!process.argv[2]) {
+		console.error('Usage: node ' + process.argv[1] + ' SERIAL_PORT');
+		process.exit(1);
+	}
+
+
+var serverPort = 8000;
+current = 0;
+var gameState = 0;
+var game = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var currentX = 0;
+var currentY = 0;
+var BOARD_WIDTH = 3;
+var BOARD_HEIGHT = 3;
+var currentCross = true;
+
+
+	//---------------------- SERIAL COMMUNICATION (Arduino) ----------------------//
+// start the serial port connection and read on newlines
+	const serial = new SerialPort(process.argv[2], {});
+	const parser = new Readline({
+		delimiter: '\r\n'
+	});
+
+	// Read data that is available on the serial port and send it to the websocket
+	serial.pipe(parser);
+	parser.on('data', function(data) {
+		console.log('Data:', data);
+		io.emit('server-msg', data);
+		if(data == "light") {
+
+			console.log('ledON');
+		}
+	});
+
+
+
+
+//---------------------- WEBAPP SERVER SETUP ---------------------------------//
+// use express to create the simple webapp
+app.use(express.static('public')); // find pages in public directory
+
+// start the server and say what port it is on
+http.listen(serverPort, function() {
+	console.log('listening on *:%s', serverPort);
+});
+//----------------------------------------------------------------------------//
+
+
+//---------------------- WEBSOCKET COMMUNICATION -----------------------------//
+// this is the websocket event handler and say if someone connects
+// as long as someone is connected, listen for messages
+io.on('connect', function(socket) {
+	console.log('a new user connected');
+	console.log(current + 1);
+	// var questionNum = 0; // keep count of question, used for IF condition.
+	socket.on('loaded', function() { // we wait until the client has loaded and contacted us that it is ready to go.
+		// setTimeout(playMusic, 0, socket);
+		// socket.emit('turn_changed', "your turn");
+		socket.username = current++;
+		socket.emit("userName", socket.username);
+		printGame();
+	});
+
+	socket.on('key_pressed', (data) => { // If we get a new message from the client we process it;
+		console.log(data + " " + socket.username);
+		action(data, socket); // run the bot function with the new message
+	});
+
+	socket.on('reset_pressed', (data) => { // If we get a new message from the client we process it;
+		console.log(data + " " + socket.username);
+		resetState(socket);
+	});
+
+	socket.on('disconnect', function() { // This function  gets called when the browser window gets closed
+		console.log('user disconnected');
+		current--;
+	});
+});
+
+function resetState(socket) {
+	gameState = 0;
+	game = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+	currentX = 0;
+	currentY = 0;
+	currentCross = true;
+
+	printGame();
+
+	var next = -1;
+	io.sockets.emit("turn_changed", {next:next, currentCross:currentCross}) ;
+
+}
+
+function action(data, socket) {
+	if (data == "play") {
+		ticTak(data, socket);
+		var next = socket.id;
+		io.sockets.emit("turn_changed", {next:next, currentCross:currentCross}) ;
+	}
+	else {
+		updateCurrentPos(data, socket);
+	}
+}
+
+function ticTak(data, socket) {
+	//updateCurrentPos(data, socket);
+
+	if (currentCross) 	game[currentY * BOARD_HEIGHT + currentX] = 1;
+	else 				game[currentY * BOARD_HEIGHT + currentX] = -1;
+
+	currentCross = !currentCross;
+	checkGame(socket);
+	printGame();
+}
+
+function nextPlayable() {
+	var i = currentY * BOARD_HEIGHT + currentX;
+	while (game[i] != 0) {
+		i++;
+
+		i = i % (BOARD_HEIGHT * BOARD_WIDTH);
+
+		currentX = i % BOARD_WIDTH;
+		currentY = Math.floor(i / BOARD_HEIGHT);
+	}
+}
+
+function updateCurrentPos(data, socket) {
+
+	switch (data) {
+		case "up":
+			currentY = (currentY - 1) % BOARD_HEIGHT;
+			break;
+		case "down":
+			currentY = (currentY + 1) % BOARD_HEIGHT;
+			break;
+		case "left":
+			currentX = (currentX - 1) % BOARD_WIDTH;
+			break;
+		case "right":
+			currentX = (currentX + 1) % BOARD_WIDTH;
+			break;
+	}
+
+	if (currentX < 0) currentX = currentX + BOARD_WIDTH;
+	if (currentY < 0) currentY = currentY + BOARD_HEIGHT;
+
+	for(var i = currentY; i < BOARD_HEIGHT + currentY; i++) {
+		for (var j = currentX; j < BOARD_WIDTH + currentX; j++) {
+
+			i = i % BOARD_WIDTH;
+			j = j % BOARD_HEIGHT;
+
+			if (game[i * BOARD_HEIGHT + j] == 0) {
+				currentX = j;
+				currentY = i;
+
+				i = BOARD_HEIGHT + currentY;
+				j = BOARD_WIDTH + currentX;
+			}
+		}
+	}
+
+	console.log("X: " + currentX + " Y: " + currentY);
+
+	printGame();
+
+}
+
+function checkGame(socket) {
+	var gameCount = 0;
+	for(var i = 0; i < 9; i++) {
+		if(game[i] != 0) gameCount++;
+	}
+
+	if(checkAllRows() || checkAllColumns() || checkAllDiagonals())
+	{
+		socket.emit("won", socket.username);
+		resetGame();
+		return;
+	}
+
+	if (gameCount == 9) {
+		resetGame();
+	}
+	else {
+		nextPlayable();
+	}
+
+	printGame();
+}
+
+function checkAllRows() {
+	var ret = false;
+
+	if(	(game[0] != 0 && (game[0] == game[1] && game[1]== game[2]))
+		|| ((game[3] != 0) && (game[3] == game[4] && game[4]== game[5]))
+		|| ((game[6] != 0) && (game[6] == game[7] && game[7]== game[8]))) {
+		ret = true;
+	}
+
+	return ret;
+}
+
+function checkAllColumns() {
+
+	var ret = false;
+
+	if(	(game[0] != 0 && (game[0] == game[3] && game[3]== game[6]))
+		|| (game[1] != 0 && (game[1] == game[4] && game[4]== game[7]))
+		|| (game[2] != 0 && (game[2] == game[5] && game[5]== game[8]))) {
+		ret = true;
+	}
+
+	return ret;
+}
+
+function checkAllDiagonals() {
+
+	var ret = false;
+
+	if(	(game[0] != 0 && (game[0] == game[4] && game[4]== game[8]))
+		|| (game[2] != 0 && (game[2] == game[4] && game[4]== game[6]))) {
+		ret = true;
+	}
+
+	return ret;
+}
+
+function resetGame() {
+	currentX = 0;
+	currentY = 0;
+	currentCross = true;
+	game = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+}
+
+function printGame() {
+	io.sockets.emit("game_updated", game, currentX, currentY, BOARD_WIDTH);
+	var game_temp = [];
+	for(i = 0; i < game.length; i++) {
+		if(game[i] == -1) game_temp[i] = 2;
+		else game_temp[i] = game[i];
+	}
+
+	serial.write(game_temp.toString() + "\n");
+}
---
+++ public/js/index.js
@@ -0,0 +1,127 @@
+// WebSocket connection setup
+var socket = io();
+
+document.getElementById('up').onclick = function () {
+  console.log('up pressed');
+  socket.emit('key_pressed', 'up');
+}
+
+document.getElementById('down').onclick = function () {
+  console.log('down pressed');
+  socket.emit('key_pressed', 'down');
+};
+
+document.getElementById('left').onclick = function () {
+  console.log('left pressed');
+  socket.emit('key_pressed', 'left');
+};
+
+document.getElementById('right').onclick = function () {
+  console.log('right pressed');
+  socket.emit('key_pressed', 'right');
+};
+
+document.getElementById('play').onclick = function () {
+  console.log('play pressed');
+  socket.emit('key_pressed', 'play');
+
+};
+
+document.getElementById('reset').onclick = function () {
+  console.log('reset pressed');
+  socket.emit('reset_pressed', 'reset');
+
+};
+
+socket.on('turn_changed', function (msg) {
+  console.log('turn changed');
+
+  if (msg.next != socket.id) {
+    document.getElementById('up').disabled = false;
+    document.getElementById('down').disabled = false;
+    document.getElementById('right').disabled = false;
+    document.getElementById('left').disabled = false;
+    document.getElementById('play').disabled = false;
+    document.getElementById("turn").innerText = "Your turn";
+    document.getElementById("turn").setAttribute("style", "color:green");
+
+    if (msg.currentCross == true) {
+      document.getElementById("type").innerText = "X";
+    }
+    else {
+      document.getElementById("type").innerText = "O";
+    }
+  }
+  else {
+    document.getElementById('up').disabled = true;
+    document.getElementById('down').disabled = true;
+    document.getElementById('left').disabled = true;
+    document.getElementById('right').disabled = true;
+    document.getElementById('play').disabled = true;
+
+    document.getElementById("turn").innerText = "Others turn";
+    document.getElementById("turn").setAttribute("style", "color:red");
+
+
+    if (msg.currentCross == true) {
+      document.getElementById("type").innerText = "O";
+    }
+    else {
+      document.getElementById("type").innerText = "X";
+    }
+  }
+
+});
+
+socket.on('connect', function() { // We let the server know that we are up and running also from the client side;
+  socket.emit('loaded');
+});
+
+socket.on('disconnect', function() { // We let the server know that we are up and running also from the client side;
+  socket.emit('disconnected');
+});
+
+socket.on('userName', function(msg) { // We let the server know that we are up and running also from the client side;
+  socket.username = msg;
+});
+
+socket.on('game_updated', function (game, currentX, currentY, board_width) {
+  for (var i = 0; i < game.length; i++) {
+    var elem = document.getElementById("b"+(i+1).toString());
+    elem.innerText =
+        (game[i] == 1)?"X":
+            (game[i] == -1)?"O":
+                ((currentY*board_width + currentX) == i)?"_":" ";
+
+    var newStyle =
+        (game[i] == 1)?"color:red":
+            (game[i] == -1)?"color:orange":
+                ((currentY*board_width + currentX) == i)?"color:black":" ";
+    elem.setAttribute("style", newStyle);
+  }
+});
+
+socket.on('won', function(msg) { // We let the server know that we are up and running also from the client side;
+  if(socket.username == msg) {
+    alert("You won!");
+  }
+});
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
